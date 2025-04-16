5a8f545d9c92abb7734178b5775cc7abf342b459
Francisca <francisca.paredes.pr@gmail.com>
2025-04-16 11:56:43 -0400

    collectstatic

--- ecoproject/settings.py
+++ ecoproject/settings.py
@@ -122,6 +122,9 @@ STATICFILES_DIRS = [
     'ecoproject/static',
 ]
 
+# Media files configuration
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
